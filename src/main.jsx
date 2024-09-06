fdb12714100b60ccb4bd279dde8f95187c71716d
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-09-07 00:18:46 +0700

    Building a Reusable Star Rating Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import StarRating from './components/StarRating'
+// import App from './App.jsx'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5}/>
+    <StarRating maxRating={10}/>
   </StrictMode>,
 )
