5a0e530abb084473ab0940fc52e86ba4bd0e3e6c
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-09-08 02:01:49 +0700

    Improving Reusability With Props

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import StarRating from './components/StarRating'
 // import App from './App.jsx'
 // import './index.css'
 
+function Test(){
+  const [movieRating, setMovieRating] = useState(0)
+
+  return(
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing' ]}/>
+    <Test/>
   </StrictMode>,
 )
