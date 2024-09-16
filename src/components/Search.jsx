dddd16463d00ce278328062965189331ffce6be8
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-09-16 23:59:38 +0700

    Refs to Select DOM Elements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+	const inputElement = useRef(null);
+
+	useEffect(() => {
+		function callback(e){
+			if(document.activeElement === inputElement.current) return;
+
+			if(e.key === 'Enter'){
+				inputElement.current.focus();
+				setQuery('');
+			}
+		}
+		document.addEventListener('keydown', callback);
+
+		return () => {
+			document.removeEventListener('keydown', callback);
+		};
+
+	},[setQuery]);
+	
 	return (
 		<input
 			className="search"
 			type="text"
 			placeholder="Search movies..."
 			value={query}
-			onChange={(e) => setQuery(e.target.value)} />
+			onChange={(e) => setQuery(e.target.value)}
+			ref={inputElement}/>
 	);
 }
